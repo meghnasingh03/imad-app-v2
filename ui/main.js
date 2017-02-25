552d3552262c4e2552ffa02cf7be44ff571aae17
meghnasingh03 <meghnaop@gmail.com>
2017-02-25 06:35:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 //move the image
 var img=document.getElementById('madi');
 img.onclick=function(){
-   var interval=set interval(moveRight,100);
+   var interval=setInterval(moveRight,100);
 };
