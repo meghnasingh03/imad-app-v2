8c613a930f2ebec5dfa20f04599fa74969f924c8
meghnasingh03 <meghnaop@gmail.com>
2017-02-26 11:26:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,4 +8,10 @@ function moveRight(){
 }
 img.onclick=function(){
    var interval=setInterval(moveRight,100);
+   
 };
+var button = document.getElementById('counter
+var count=0;
+button.onclick = function(){
+    
+}
