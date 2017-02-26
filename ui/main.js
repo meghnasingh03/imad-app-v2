ba19dd34f14e7fe03340a39b1ccab3f15f077cfd
meghnasingh03 <meghnaop@gmail.com>
2017-02-26 19:31:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,8 +10,11 @@ img.onclick=function(){
    var interval=setInterval(moveRight,100);
    
 };
-var button = document.getElementById('counter
-var count=0;
+var button = document.getElementById('counter');
+var counter=0;
 button.onclick = function(){
+   counter=counter+1;
+   var span=document.getElementById('count');
+   span.innerHTML=counter.toString();
     
 }
