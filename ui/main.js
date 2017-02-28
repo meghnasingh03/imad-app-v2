84a4595780cecaee5b75865a64db27d8f93a7117
meghnasingh03 <meghnaop@gmail.com>
2017-02-28 06:37:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,7 @@ img.onclick=function(){
 var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function(){
+    var request= new XMLHttpRequest();
     request.onreadystatechange=function(){
     if(request.readyState == XMLHttpRequest.DONE){
     if(request.status==200){
